940431357dcfeab55b9403bff4f1d829eddb71c8
ecuriotto <7021326+ecuriotto@users.noreply.github.com>
2024-01-29 16:31:41 +0100

    moved auth config params on external file

--- src/main/java/com/camunda/academy/PaymentApplication.java
+++ src/main/java/com/camunda/academy/PaymentApplication.java
@@ -1,6 +1,9 @@
 package com.camunda.academy;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 import java.util.Scanner;
 
 import com.camunda.academy.handler.PDFCreatorHandler;
@@ -13,11 +16,12 @@ import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProviderBuilder;
 public class PaymentApplication {
 
 	// Zeebe Client Credentials
-    private static final String ZEEBE_ADDRESS = "c7e94005-c52d-40f2-b636-2f064700ac54.dsm-1.zeebe.camunda.io:443";
-    private static final String ZEEBE_CLIENT_ID = "Et6udCeUudspLu4xpERF1cSr3ObPM5T4";
-    private static final String ZEEBE_CLIENT_SECRET = "FJEq.H4BCFU-Tz~7ipoGaVxPIrk9VDQgonjYzLn9MaxbDa1XAWkPMYrbuVY2T~gf";
-    private static final String ZEEBE_AUTHORIZATION_SERVER_URL = "https://login.cloud.camunda.io/oauth/token";
-    private static final String ZEEBE_TOKEN_AUDIENCE = "zeebe.camunda.io";      
+    private static final String ZEEBE_PROPERTIES_PATH = "src/main/resources/application.properties";
+    private static String ZEEBE_ADDRESS;
+    private static String ZEEBE_CLIENT_ID;
+    private static String ZEEBE_CLIENT_SECRET;
+    //private static String ZEEBE_AUTHORIZATION_SERVER_URL;
+    private static String ZEEBE_TOKEN_AUDIENCE = "zeebe.camunda.io";  
 
 	// Process instance creation
 	private static final String PROCESS_ID = "pdf-creator";	
@@ -28,24 +32,24 @@ public class PaymentApplication {
 	private static final int NUM_THREADS = 2; // Default 1
 
 	// Worker configuration
-	private static final int POLL_INTERVAL = 5; 			// Max interval to check for new jobs
-	private static final int WORKER_REQUEST_TIMEOUT = 0; 	// Set the request timeout for activate job request used to poll for new job. Default 0. if the requestTimeout < 0, long polling is disabled and the request is completed immediately, even when no job is activated
-	private static final int WORKER_TIMEOUT = 5; 		// Set the time for how long a job is exclusively assigned for this worker.
+	private static final int POLL_INTERVAL = 7000; 			// Max interval to check for new jobs
+	private static final int WORKER_REQUEST_TIMEOUT = 8000; 	// Set the request timeout for activate job request used to poll for new job. Default 0. if the requestTimeout < 0, long polling is disabled and the request is completed immediately, even when no job is activated
+	private static final int WORKER_TIMEOUT = 5000; 		// Set the time for how long a job is exclusively assigned for this worker.
 	private static final int WORKER_MAX_JOBS_ACTIVATE = 2; // IS THIS REDUNDANT? CLIENT SIDE AND WORKER SIDE? This takes priority
 	private static final boolean STREAM_ENABLED = false; 	// If set as enabled, the job worker will use a mix of streaming and polling to activate jobs
 	private static final int STREAM_TIMEOUT = 10000;		// If streaming is enabled, sets a maximum lifetime for a given stream.
 
 
 	public static void main(String[] args) {
-
+        loadProperties();
 		final OAuthCredentialsProvider credentialsProvider = new OAuthCredentialsProviderBuilder()
-			.authorizationServerUrl(ZEEBE_AUTHORIZATION_SERVER_URL)
 			.audience(ZEEBE_TOKEN_AUDIENCE)
 			.clientId(ZEEBE_CLIENT_ID)
 			.clientSecret(ZEEBE_CLIENT_SECRET)
 			.build();
 
 		try (final ZeebeClient client = ZeebeClient.newClientBuilder()
+		
 		     	.gatewayAddress(ZEEBE_ADDRESS)
 		     	.credentialsProvider(credentialsProvider)
 				.defaultJobWorkerMaxJobsActive(MAX_JOBS_ACTIVATE)
@@ -59,7 +63,7 @@ public class PaymentApplication {
 			startProcessInstances(client, NUM_INSTANCES);
 
 			// Start a Job Worker			
-			final JobWorker PDFCreatorWorker = client.newWorker()
+			final JobWorker pDFCreatorWorker = client.newWorker()
 				.jobType("pdfCreation")
 				.handler(new PDFCreatorHandler())
 				//.fetchVariables(args)
@@ -75,11 +79,26 @@ public class PaymentApplication {
 			Scanner sc = new Scanner(System.in);			
 			sc.nextInt();
 			sc.close();
-			PDFCreatorWorker.close();
+			pDFCreatorWorker.close();
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static void loadProperties() {
+       Properties properties = new Properties();
+        try (FileInputStream input = new FileInputStream(ZEEBE_PROPERTIES_PATH)) {
+            properties.load(input);
+
+            ZEEBE_ADDRESS = properties.getProperty("zeebe.client.cloud.address");
+            ZEEBE_CLIENT_ID = properties.getProperty("zeebe.client.cloud.clientId");
+            ZEEBE_CLIENT_SECRET = properties.getProperty("zeebe.client.cloud.clientSecret");
+            //ZEEBE_AUTHORIZATION_SERVER_URL = properties.getProperty("authorization.server.url");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 	}
 
 	public static void startProcessInstances(ZeebeClient zeebeClient, int numInstances) {
